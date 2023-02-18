eda61c892a880371479871e23d2e295159f199ab
techswarn <tech.swarns@gmail.com>
2023-02-19 05:06:43 +0530

    code update

--- packages/sample/func/index.js
+++ packages/sample/func/index.js
@@ -1,31 +1,16 @@
-//const name = require("./name")
-const { Configuration, OpenAIApi } = require("openai");
+const name = require("./name")
+
 // import name from "./../../../lib/name.js"
 
-// console.log(name)
+console.log(name)
 
 async function main(args) {
-    // console.log('-----------Logging values-----')
-    // console.log(args?.__ow_body);
-    // console.log(args?.__ow_headers);
-    // console.log('-----------Logging values-----')
-
-
-    try {
-      const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-      });
-      const openai = new OpenAIApi(configuration);
-      
-      const completion = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: "Hello world",
-      });
-      console.log(completion.data.choices[0].text);
-
-    }catch(err){
-      console.log(err)
-    }
+    console.log('-----------Logging values-----')
+    console.log(args?.__ow_body);
+    console.log(args?.__ow_headers);
+    console.log(args?.__ow_body);
+    console.log(args?.__ow_path);
+    console.log('-----------Logging values-----')
 
     let message = 'Function executed'
     return {"body": message}
