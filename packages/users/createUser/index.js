719208b99351edfe1833e08240227355fd02a1a7
techswarn <tech.swarns@gmail.com>
2023-05-02 13:26:31 +0530

    code update

--- packages/users/createUser/index.js
+++ packages/users/createUser/index.js
@@ -20,13 +20,27 @@ async function main(args) {
     console.log(args?.__ow_headers);
     console.log(args?.__ow_path);
     console.log('-----------Logging values-----')
+    if(!args?.__ow_body) {
+      return  {
+        statusCode: 404,
+        body: "Request object not found"
+      }
+    }
+    const reqBody = JSON.parse(args?.__ow_body)
+    
+    if(reqBody?.name === "") {
+      return  {
+        statusCode: 404,
+        body: "name missing in request body"
+      }
+    }
 
     try{
-      const swarn = await Person.create({name: "Swarn"})
-      console.log(swarn)
+      const user = await Person.create({name: reqBody?.name})
+      console.log(user)
       return {
         statusCode : 200,
-        body: swarn
+        body: `${user.name} created`
       };
     }
     catch(err){
