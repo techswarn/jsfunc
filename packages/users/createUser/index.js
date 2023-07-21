352eca39a8e16862e3f0dd08016332c40d291b31
techswarn <tech.swarns@gmail.com>
2023-07-21 12:36:25 +0530

    code update

--- packages/users/createUser/index.js
+++ packages/users/createUser/index.js
@@ -1,63 +1,51 @@
-const name = require("./name")
-const mongoose = require('mongoose');
-const { BinaryReader } = require('@protobuf-ts/runtime')
+const name = require("./name");
+const mongoose = require("mongoose");
+const { BinaryReader } = require("@protobuf-ts/runtime");
 
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
-dotenv.config({path: './../config.env'})
-console.log(process.env.LOG_DESTINATIONS)
+dotenv.config({ path: "./../config.env" });
+console.log(process.env.LOG_DESTINATIONS);
 
-const dbConnect = require("./db")
+const dbConnect = require("./db");
 
-console.log(name)
+console.log(name);
 
-dbConnect()
+dbConnect();
 
 const PersonSchema = new mongoose.Schema({
-  name: String
+  name: String,
 });
 
-const Person = mongoose.model('Person', PersonSchema);
+const Person = mongoose.model("Person", PersonSchema);
 
-async function main(args) {
-    console.log('-----------Logging values-----')
-    console.log(args?.__ow_body);
-    console.log(args?.__ow_headers);
-    console.log(args?.__ow_path);
-    console.log('-----------Logging values-----')
-    const reader = new BinaryReader(Buffer.from("TEST"));
-    console.log(reader.len);
+async function main(event, context) {
+  console.log("-----------Logging values-----");
+  console.log(event);
+  console.log(context);
+  console.log("-----------Logging values-----");
 
-    if(!args?.__ow_body) {
-      return  {
-        statusCode: 404,
-        body: "Request object not found"
-      }
-    }
-    const reqBody = JSON.parse(args?.__ow_body)
-    
-    if(reqBody?.name === "") {
-      return  {
-        statusCode: 404,
-        body: "name missing in request body"
-      }
-    }
-
-    try{
-      const user = await Person.create({name: reqBody?.name})
-      console.log(user)
-      return {
-        statusCode : 200,
-        body: `${user.name} created`
-      };
-    }
-    catch(err){
-      console.log(err)
-      return  {
-        statusCode: 500,
-        body: err.message
-      }
-    }
+  if (!event.name) {
+    return {
+      statusCode: 404,
+      body: "name missing in request body",
+    };
+  }
+  const username = event.name;
+  try {
+    const user = await Person.create({ name: username });
+    console.log(user);
+    return {
+      statusCode: 200,
+      body: `${user.name} created`,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: err.message,
+    };
+  }
 }
 
-exports.main = main
+exports.main = main;
