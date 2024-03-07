3a88e5f43d30debd917047d28da29e950929ce74
techswarn <tech.swarns@gmail.com>
2024-03-07 17:02:47 +0530

    code update

--- packages/webinvoke/invoke/index.js
+++ packages/webinvoke/invoke/index.js
@@ -1,16 +1,45 @@
 const sgMail = require("@sendgrid/mail");
 
+// Assuming SENDGRID_API_KEY and SENDER_EMAIL are set as environment variables
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 async function main(event, context) {
   console.log(event);
   console.log(context);
+  // const details = {
+  //   to: "s.swarn619@gmail.com",
+  //   templateId: "Some ramdom id",
+  //   dynamicData: "SOME DATA",
+  //   unsubscribeGroupId: "SOME DATA",
+  //   emergencyByPass: "SOME DATA",
+  // };
+  // // Construct the email object
+  // const msg = {
+  //   to: details.to,
+  //   from: process.env.SENDER_EMAIL,
+  //   template_id: details.templateId,
+  //   dynamic_template_data: details.dynamicData,
+  //   mail_settings: {
+  //     bypass_list_management: { enable: details.emergencyByPass },
+  //   },
+  //   ...(details.unsubscribeGroupId
+  //     ? { asm: { group_id: details.unsubscribeGroupId } }
+  //     : {}),
+  // };
 
-  return {
-    body: {
-      name: "Swan",
-      email: "admin@swarn1.com",
-    },
-    statusCode: 201,
-  };
+  try {
+    // Send the email using SendGrid
+    //   await sgMail.send(msg);
+    return {
+      body: "testing",
+      statusCode: 201,
+    };
+  } catch (error) {
+    console.error(error);
+    response = {
+      body: { error: "Failed to send email", details: error.message },
+    };
+  }
 }
 
 exports.main = main;
